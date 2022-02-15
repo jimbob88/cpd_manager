cdb91e68a1e04b1603cb7b6bf7372ed27768c4cf
James Blackburn <blackburnfjames@gmail.com>
2022-02-15 15:43:30 +0000

    Added input validation

--- main.py
+++ main.py
@@ -4,18 +4,30 @@ from prompt_toolkit.formatted_text import HTML
 from prompt_toolkit.styles import Style
 from prompt_toolkit.shortcuts import set_title
 from prompt_toolkit.completion import WordCompleter
+from prompt_toolkit.validation import Validator
+import re
 
-style = Style.from_dict({"view": "#0000FF underline", "add": "#00FF00 underline",})
 
 if __name__ == "__main__":
     set_title("CPD Manager")
-    view_add = WordCompleter(["View", "Add"])
+
     answer = prompt(
         HTML("Would you like to <view>View</view> or <add>Add</add> to your CPD? "),
-        completer=view_add,
-        style=style,
+        completer=WordCompleter(["View", "Add"]),
+        style=Style.from_dict(
+            {"view": "#0000FF underline", "add": "#00FF00 underline",}
+        ),
     )
     if answer.lower() == "view":
         pass
-    elif answer.lower() == "Add":
-        pass
+
+    elif answer.lower() == "add":
+        prompt(
+            HTML("<when>When</when> did you complete this learning (DD/MM/YYYY)? "),
+            validator=Validator.from_callable(
+                lambda x: re.search(r"\d{2}/\d{2}/\d{4}", x),
+                error_message="Not a valid date (Must be in the form DD/MM/YYYY).",
+                move_cursor_to_end=True,
+            ),
+        )
+
